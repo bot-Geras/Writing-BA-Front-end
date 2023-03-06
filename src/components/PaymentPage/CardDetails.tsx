5f5cfb1f9fba50685cb43a67806410d4f0481115
Brian <briandave771@gmail.com>
2023-03-06 04:09:07 +0300

    Refactor styles for payment form

--- src/components/PaymentPage/CardDetails.tsx
+++ src/components/PaymentPage/CardDetails.tsx
@@ -19,13 +19,16 @@ const CardDetails: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center mx-auto h-screen w-4/5 xl:w-[40%] xl:ml-auto xl:mr-0">
-      <div className="container">
-        <div className=" h-[95%]">
+    <div
+      // className="flex items-center mx-auto h-screen w-4/5 xl:w-[90%] xl:ml-auto xl:mr-0"
+      className="h-screen w-4/5 xl:w-[40%] xl:ml-auto "
+    >
+      <div className="container relative">
+        <div>
           {/* logo */}
           <img
-            //data-aos="fade-down"
-            className="mt-[29px] mb-[100px]"
+            data-aos="fade-down"
+            className="mb-[50px]"
             src={logo}
             alt="logo"
           />
@@ -37,11 +40,16 @@ const CardDetails: React.FC = () => {
             >
               Payment
             </h2>
+            <h4
+              className="font-[600] mb-[16px] capitalize xl:max-w-[311px] text-center md:text-left text-xl md:text-[36px] md:leading-[48px] text-[#0D1259]"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              Card details
+            </h4>
           </div>
         </div>
         <div className=" mt-10" data-aos="fade-up" data-aos-delay="600">
-          {/* <input placeholder="Enter Card Name" />
-      <input placeholder="Enter card number" /> */}
           <Formik
             initialValues={initialValues}
             onSubmit={(values: PaymentFormValues) => {}}
@@ -62,8 +70,8 @@ const CardDetails: React.FC = () => {
               } = props;
               return (
                 <Form>
-                  <div className=" md:flex md:flex-col">
-                    <div className="mb-5 l">
+                  <div className="flex flex-col">
+                    <div className="">
                       <label
                         className="xl:max-w-[90px] font-[500] text-left text-[12px] leading-[20px] text-[#0D1259] flex"
                         htmlFor="card-name"
@@ -74,19 +82,20 @@ const CardDetails: React.FC = () => {
 
                     <input
                       id="card-name"
-                      placeholder="   Enter Card name"
-                      className=" flex  h-12 w-full xl:w-[70%] bg-white border border-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px]"
-                      onChange={handleChange}
+                      placeholder=" Enter Card name"
+                      className="bg-white h-10 mb-[24px] border border-[#C4C4C4] text-slate-600 w-64 md:w-[386px] placeholder-[#C4C4C4] rounded-[5px]"
                       onBlur={handleBlur}
                     />
                   </div>
+
                   {touched.cardName && errors.cardName && (
                     <p className="text-red-500">{errors.cardName}</p>
                   )}
+
                   <div className=" mt-5 md:flex md:flex-col">
                     <div className="mb-5">
                       <label
-                        className="xl:max-w-[60px] font-[500] text-left text-[12px] leading-[20px] text-[#0D1259] flex"
+                        className="xl:max-w-[90px] font-[500] text-left text-[12px] leading-[20px] text-[#0D1259] flex"
                         htmlFor="card-number"
                       >
                         Card number
@@ -94,8 +103,8 @@ const CardDetails: React.FC = () => {
                     </div>
                     <input
                       id="card-number"
-                      placeholder="Enter Card Number"
-                      className=" h-12 w-full xl:w-[70%] bg-white border border-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px] "
+                      placeholder=" Enter Card Number"
+                      className="bg-white h-10 mb-[24px] border border-[#C4C4C4] text-slate-600 w-64 md:w-[386px] placeholder-[#C4C4C4] rounded-[5px]"
                       onChange={handleChange}
                       onBlur={handleBlur}
                     />
@@ -103,11 +112,12 @@ const CardDetails: React.FC = () => {
                       <p className="text-red-500">{errors.cardNumber}</p>
                     )}
                   </div>
-                  <div className="flex justify-center items-center">
-                    <div className="mt-5 md:flex md:flex-col">
+
+                  <div className="mt-5 flex flex-col md:justify-between  md:flex-row  ">
+                    <div className="flex flex-col items-start">
                       <label
                         htmlFor="code"
-                        className="xl:max-w-[60px] font-[500] text-left text-[12px] leading-[20px] text-[#0D1259] flex"
+                        className="font-[500]  text-left text-[12px] leading-[20px] text-[#0D1259]"
                       >
                         Security code
                       </label>
@@ -116,48 +126,60 @@ const CardDetails: React.FC = () => {
                         placeholder=" CVV"
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        className="h-12 w-full xl:w-[20%] bg-white border border-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px]"
+                        className="h-12 xl:max-w-[50px]   bg-white border border-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px]"
                       />
                     </div>
-                    <div className="flex flex-col">
-                    <label className="xl:max-w-[60px] font-[500] text-left text-[12px] leading-[20px] text-[#0D1259] flex">Expiration Date</label>
-                    <div className="flex gap-2">
-                      
-                    <div>
-                      <select className="h-12 w-full xl:w-[50%] bg-white border border-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px]">
-                        <option selected>Month</option>
-                        {[1,2,3,4,5,6,7,8,9,10,11,12].map((m,i) => (
-                          <option value={m}> {m < 10 ? `0${m}` : m}</option>
-                        ))}
-                      </select>
-                    </div>
-                    <div>
-                      <select className="h-12 w-full xl:w-[60%] bg-white border border-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px]">
-                        <option selected>Year</option>
-                        {[
-                        2020,
-                        2021,
-                        2023,
-                        2024,
-                        2025,
-                        2026,
-                        2027,
-                        2028,
-                        2029,
-                        2030,
-                        2031,
-                        2032
-                      ].map((y,i) => (
-                        <option value={y}>{y}</option>
-                      ))}
-                      </select>
-                    </div>
-                    </div>
+
+                    <div className="flex flex-col justify-between">
+                      <label className=" font-[500] text-left text-[12px] leading-[20px] text-[#0D1259]">
+                        Expiration Date
+                      </label>
+                      <div className="flex gap-2 ">
+                        <div>
+                          <select className="h-12 xl:max-w-[130px] bg-white border border-[#C4C4C4] text-slate-600  text-[#C4C4C4] rounded-[5px]">
+                            <option selected>Month</option>
+                            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(
+                              (m, i) => (
+                                <option value={m}>
+                                  {m < 10 ? `0${m}` : m}
+                                </option>
+                              )
+                            )}
+                          </select>
+                        </div>
+                        <div className="border border-solid border-1 border-blue-900 transform skew-y-12 rotate-2"></div>
+                        <div>
+                          <select className="h-12   bg-white border text-[#C4C4C4] text-slate-600  placeholder-[#C4C4C4] rounded-[5px]">
+                            <option selected>Year</option>
+                            {[
+                              2020, 2021, 2023, 2024, 2025, 2026, 2027, 2028,
+                              2029, 2030, 2031, 2032,
+                            ].map((y, i) => (
+                              <option value={y}>{y}</option>
+                            ))}
+                          </select>
+                        </div>
+                      </div>
                     </div>
                   </div>
                   {touched.cvv && errors.cvv && (
                     <p className="text-red-500">{errors.cvv}</p>
                   )}
+
+                  <div
+                    className="mt-5 flex flex-col gap-5 md:flex-row md:justify-between md:items-center"
+                    data-aos="fade-up"
+                  >
+                    <button
+                      className="rounded-[5px] bg-[#515AE5] text-white py-[12px] px-[24px] font-[700] text-[18px] leading-[24px]"
+                      type="submit"
+                    >
+                      Continue
+                    </button>
+                    <p className="underline font-[400] text-[12px] leading-[18px] text-[#515AE5]">
+                      Cancel
+                    </p>
+                  </div>
                 </Form>
               );
             }}
