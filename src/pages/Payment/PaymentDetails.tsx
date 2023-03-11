072b136f9609ddb45e2450692b4d13a853bf83d9
Brian <briandave771@gmail.com>
2023-03-11 05:17:03 +0300

    Fix payment UX and Create cancel payment routing

--- src/pages/Payment/PaymentDetails.tsx
+++ src/pages/Payment/PaymentDetails.tsx
@@ -1,21 +1,21 @@
 import Aos from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 
 import PaymentForm from "../../components/Payment/PaymentForm";
-import { QuickInfo } from "../../components/SignUp";
+import { QuickInfo } from "../../components/Payment/QuickInfo";
 
 export const PaymentDetails = () => {
-    Aos.init({
-        duration: 1800,
-        offset: 0,
-    })
+  Aos.init({
+    duration: 1800,
+    offset: 0,
+  });
 
-    return (
-        <main>
-            <div className="bg-gradient-to-b lg:space-x-[190px] md:px-20 px-10 lg:px-[130px] lg:flex lg:items-center from-[#F0F5FF] to-[#FFFFFF]">
-             <PaymentForm />
-                <QuickInfo />
-            </div>
-        </main>
-    )
-}
+  return (
+    <main>
+      <div className="flex xl:justify-center xl:items-center bg-gradient-to-b lg:space-x-[190px] md:px-20 px-10 lg:px-[130px] lg:flex lg:items-center from-[#F0F5FF] to-[#FFFFFF]">
+        <PaymentForm />
+        <QuickInfo />
+      </div>
+    </main>
+  );
+};
