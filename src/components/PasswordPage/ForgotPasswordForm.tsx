31d1225c2c2c558cb4077962b33cab7e23dad877
Brian <briandave771@gmail.com>
2023-03-17 11:04:22 +0300

    Refactor the styles and route cancel to login page

--- src/components/PasswordPage/ForgotPasswordForm.tsx
+++ src/components/PasswordPage/ForgotPasswordForm.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import logo from "../../assets/images/wbaLogo.png";
-import { Formik, FormikProps, Form, Field } from "formik";
+import { Formik, FormikProps, Form } from "formik";
 import { string, object } from "yup";
+import { useNavigate } from "react-router-dom";
 interface ResetPasswordFormValues {
   email: string;
 }
@@ -9,42 +10,50 @@ const ForgotPasswordForm: React.FC = () => {
   const initialValues: ResetPasswordFormValues = {
     email: "",
   };
+  const navigate = useNavigate();
+  const goBackToLoginPage = (): void => {
+    navigate(`/login`);
+  };
   return (
-    <div className="flex flex-col   items-center h-screen w-4/5 md:my-20   xl:w-[40%] xl:mx-0">
-      <div className="container w-[386px] ">
+    <div className="flex flex-col justify-center items-center h-screen w-4/5 xl:w-[40%] xl:mx-0">
+      <div className="container w-[386px]  ">
         <div>
           {/* logo */}
           <div
-            className="mt-10 flex items-center justify-between md:items-center  md:justify-between mb-[30px] "
+            className=" flex items-center justify-between md:items-center  md:justify-between mb-[30px] "
             data-aos="fade-down"
           >
             <img className="" src={logo} alt="logo" />
           </div>
 
-          <div className="flex flex-col items-center md:items-start">
+          <div>
             <h2
-              className="xl:max-w-[223px] md:text-left mb-[16px] font-[700] text-5xl md:text-[56px] md:leading-[64px] text-[#0D1259]"
+              className="md:text-left mb-[1em] font-[700] text-3xl md:text-4xl md:leading-[64px] text-[#0D1259]"
               data-aos="fade-up"
               data-aos-delay="300"
             >
-              Forgot password
+              Forgot Password
             </h2>
           </div>
+          <div
+            className="flex flex-col md:flex md:mt-6 md:flex-col items-center md:items-start md:gap-5"
+            data-aos="fade-up"
+          >
+            <p className="xl:max-w-[25em] text-center  md:text-left font-[700] text-[16px] leading-[24px] text-[#0D1259]">
+              Please enter your email address so as to reset your password
+            </p>
+            <p className="xl:max-w-[25em] text-center  md:text-left font-[400] text-[16px] leading-[24px] text-[#0D1259]">
+              A link will be sent to your mailbox
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col md:flex md:mt-6 md:flex-col items-center md:items-start md:gap-5" >
-                    <p className="xl:max-w-[25em] text-center  md:text-left font-[400] text-[16px] leading-[24px] text-[#0D1259]">
-                    Please enter your email address so as to reset your password
-                    </p>
-                    <p className="xl:max-w-[25em] text-center  md:text-left font-[400] text-[16px] leading-[24px] text-[#0D1259]">
-                    A link will be sent to your mailbox
-                    </p>
-                  </div>
+
         <div className=" mt-10" data-aos="fade-up" data-aos-delay="600">
           <Formik
             initialValues={initialValues}
             onSubmit={(values: ResetPasswordFormValues) => {}}
             validationSchema={object({
-              email: string().email().required("enter your email"),
+              email: string().email().required("enter your Email Address here"),
             })}
           >
             {(props: FormikProps<ResetPasswordFormValues>) => {
@@ -70,7 +79,7 @@ const ForgotPasswordForm: React.FC = () => {
                     </div>
 
                     <input
-                      id="card-name"
+                      id="email"
                       placeholder=" Enter email address"
                       className="bg-white h-10 border border-[#C4C4C4] text-slate-600 w-64 md:w-[386px] placeholder-[#C4C4C4] rounded-[5px]"
                       onBlur={handleBlur}
@@ -81,7 +90,6 @@ const ForgotPasswordForm: React.FC = () => {
                   {touched.email && errors.email && (
                     <p className="text-red-500">{errors.email}</p>
                   )}
-                  
 
                   <div
                     className="mt-5 flex flex-col gap-5 md:flex-row md:justify-between md:items-center xl:w-[386px]"
@@ -93,7 +101,10 @@ const ForgotPasswordForm: React.FC = () => {
                     >
                       Continue
                     </button>
-                    <p className="underline font-[400] text-[12px] leading-[18px] text-[#515AE5]">
+                    <p
+                      className="underline font-[400] text-[12px] leading-[18px] text-[#515AE5] cursor-pointer"
+                      onClick={goBackToLoginPage}
+                    >
                       Cancel
                     </p>
                   </div>
